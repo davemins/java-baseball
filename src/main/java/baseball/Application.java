c305c0fe3accc485a5253730762a46698cd4c064
davemins <davemins4125@naver.com>
2023-09-20 15:10:58 +0900

    chore : 주석 추가 - 문제 요약

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
+/*
+같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼,
+같은 수가 전혀 없으면 낫싱이란 힌트를 얻고,
+그 힌트를 이용해서 먼저 상대방(컴퓨터)의 수를 맞추면 승리
+indent depth를 3이 넘지 않도록 구현
+3항 연산자를 쓰지 않는다. -> for문을 레벨3으로 구현할 수도 있어서 그런 듯
+최대한 객체지향적으로 프로그래밍 해라
+라이브러리 제공한 것만 사용
+ */
+
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 }
